7f76c1e45d85635fb6085b0ee14e9b0b490f10ac
Rominos111 <romain.theodet@hotmail.fr>
2020-07-31 23:40:48 +0200

    add: Commit initial

---
+++ include/main.h
@@ -0,0 +1,9 @@
+// Created by Romain on 31/07/2020.
+
+#ifndef AUTOCORRECT_LEVENSHTEIN_MAIN_H
+#define AUTOCORRECT_LEVENSHTEIN_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif //AUTOCORRECT_LEVENSHTEIN_MAIN_H
---
+++ src/main.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Created by Romain on 31/07/2020.
+
+#include "main.h"
+
+int main() {
+    return EXIT_SUCCESS;
+}
