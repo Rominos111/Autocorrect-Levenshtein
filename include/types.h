2c70a9055111cf1c23a185f78eb3ed9466309665
Rominos111 <romain.theodet@hotmail.fr>
2020-08-05 20:45:57 +0200

    new: Fichiers

---
+++ include/types.h
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Created by Romain on 01/08/2020.
+
+#ifndef AUTOCORRECT_LEVENSHTEIN_TYPES_H
+#define AUTOCORRECT_LEVENSHTEIN_TYPES_H
+
+#endif //AUTOCORRECT_LEVENSHTEIN_TYPES_H
---
+++ src/dictionary.c
@@ -0,0 +1,2 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Created by Romain on 01/08/2020.
